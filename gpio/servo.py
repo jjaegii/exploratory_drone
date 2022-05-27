b51546635f8123d60a13ffbdb93c2ef090731a43
jjaegii <hn06038@gmail.com>
2022-05-28 02:03:18 +0900

    서보모터 테스트

--- gpio/servo.py
+++ gpio/servo.py
@@ -6,9 +6,18 @@ servo_pin = 18
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(servo_pin,GPIO.OUT)
 pwm = GPIO.PWM(servo_pin,50)
-GPIO.cleanup()
+pwm.start(3.0)
+
 #pwm.start(3.0)
 #timeA = 0.6
 
-pwm.ChangeDutyCycle(float(sys.argv[1]))
+#print(float(sys.argv[1]))
+#pwm.ChangeDutyCycle(3.0)
+#time.sleep(1.0)
 
+pwm.ChangeDutyCycle(float(sys.argv[1]))   # 서보모터를 0도로 회전(이동)
+time.sleep(1.0)   
+pwm.ChangeDutyCycle(0.0)
+
+pwm.stop()
+GPIO.cleanup()
