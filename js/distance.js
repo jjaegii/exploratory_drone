9a5dc5a47a047cc623bf59b01352ff5e322450e1
jjaegii <hn06038@gmail.com>
2022-05-29 04:44:39 +0900

    추가 : ajax 적용

---
+++ js/distance.js
@@ -0,0 +1,14 @@
+function distance() {
+    const xhr = new XMLHttpRequest(); // XMLHttpRequest 인스턴스의 open(), send() 메서드를 사용하기 위해 인스턴스를 생성한다.
+    
+    xhr.open('GET', '/distance', true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send();
+
+    xhr.addEventListener('load', function() {
+        console.log(xhr.responseText);
+        document.querySelector('#distance').innerHTML = xhr.responseText;
+    })
+}
+
+setInterval(distance, 3000);
