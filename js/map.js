d62d357526eac1d775bf51023011bc2290dd7aed
jjaegii <hn06038@gmail.com>
2022-05-28 18:13:58 +0900

    지도 추가

---
+++ js/map.js
@@ -0,0 +1,12 @@
+function myMap(){
+      var mapOptions = { 
+            center:new google.maps.LatLng(35.83063, 128.75475),
+            zoom:18,
+            disableDefaultUI:true,
+            mapTypeId: google.maps.MapTypeId.HYBRID
+      };
+  
+      var map = new google.maps.Map(document.getElementById("googleMap") 
+            , mapOptions );
+      
+   }
